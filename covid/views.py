9627e2759df75d3070a9c21d8c3d912bf7dac31e
aravindha1234u <aravindha1234u@gmail.com>
2020-04-10 15:32:10 +0530

    Statewise data and graph

--- covid/views.py
+++ covid/views.py
@@ -4,17 +4,16 @@ from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User, auth
 from django.views.decorators.csrf import csrf_exempt
-import requests, json
+import requests
 import datetime
 from bs4 import BeautifulSoup
 
 def index(request):
-  
+
   response = requests.get('https://api.covid19india.org/data.json')
+  resp = response.json()
 
  # India count
-
-  resp = response.json()
 
   statewise = resp['statewise']
 
@@ -48,9 +47,9 @@ def index(request):
   recovered_tamilnadu = tamilnadu['recovered']
 
   dead_tamilnadu = tamilnadu['deaths']
- 
+
  #world count
-  
+
   world_response = requests.get('https://www.worldometers.info/coronavirus/')
 
 
@@ -72,6 +71,8 @@ def index(request):
   daily_cases = resp['cases_time_series']
   tot_length = len(daily_cases)
 
+  last_updated_time = resp['statewise'][0]['lastupdatedtime']
+
   today_case = int(resp['statewise'][0]['deltaconfirmed'])
 
   sterday_confirmed = int(daily_cases[tot_length-1]['dailyconfirmed'])
@@ -84,7 +85,45 @@ def index(request):
 
   sterday4_confirmed = int(daily_cases[tot_length-5]['dailyconfirmed'])
 
-# Time calculation 
+  #--------------------------------------------------------------------------
+  today_death = int(resp['statewise'][0]['deltadeaths'])
+
+  sterday_death = int(daily_cases[tot_length-1]['dailydeceased'])
+
+  sterday1_death = int(daily_cases[tot_length-2]['dailydeceased'])
+
+  sterday2_death = int(daily_cases[tot_length-3]['dailydeceased'])
+
+  sterday3_death = int(daily_cases[tot_length-4]['dailydeceased'])
+
+  sterday4_death = int(daily_cases[tot_length-5]['dailydeceased'])
+
+   #--------------------------------------------------------------------------
+  today_recovered = int(resp['statewise'][0]['deltarecovered'])
+
+  sterday_recovered = int(daily_cases[tot_length-1]['dailyrecovered'])
+
+  sterday1_recovered = int(daily_cases[tot_length-2]['dailyrecovered'])
+
+  sterday2_recovered = int(daily_cases[tot_length-3]['dailyrecovered'])
+
+  sterday3_recovered = int(daily_cases[tot_length-4]['dailyrecovered'])
+
+  sterday4_recovered = int(daily_cases[tot_length-5]['dailyrecovered'])
+
+ #--------------------------------------------------------------------------
+
+  #statewise daily Changes
+  state=requests.get("https://api.covid19india.org/states_daily.json")
+  s=state.json()
+  s=s['states_daily']
+  y1=int(s[len(s) -1]['tn'])
+  y2=int(s[len(s) -2]['tn'])
+  y3=int(s[len(s) -3]['tn'])
+  y4=int(s[len(s) -4]['tn'])
+  y5=int(s[len(s) -5]['tn'])
+
+# Time calculation
 
   tdy = datetime.datetime.today()
   today = (tdy.strftime("%d")+' '+(tdy.strftime("%B")))
@@ -112,8 +151,7 @@ def index(request):
   districtwise = districtwise.json()
 
   tn_districts = districtwise['Tamil Nadu']['districtData']
-  
-  tot_districts = len(tn_districts)
+
   t=tn_districts
   key=0
   final={}
@@ -131,8 +169,23 @@ def index(request):
 				  key=i
 	  final[ar[key]]=t[ar[key]]
 	  del t[ar[key]]
-	
   tn_districts=final
 
-  return render(request, 'index.html',{'total_india':total_india,'active_india':active_india,'recovered_india':recovered_india,'dead_india':dead_india,'updated_time_india':updated_time_india, 'total_tamilnadu':total_tamilnadu,'active_tamilnadu':active_tamilnadu,'recovered_tamilnadu':recovered_tamilnadu,'dead_tamilnadu':dead_tamilnadu, 'total_world':total_world, 'world_active_cases':world_active_cases, 'total_world_death':total_world_death,'total_world_recovered':total_world_recovered,'today':today,'yesterday':yesterday,'yesterday1':yesterday1,'yesterday2':yesterday2,'yesterday3':yesterday3,'yesterday4':yesterday4,'today_case':today_case,'sterday_confirmed':sterday_confirmed,'sterday1_confirmed':sterday1_confirmed,'sterday2_confirmed':sterday2_confirmed,'sterday3_confirmed':sterday3_confirmed,'sterday4_confirmed':sterday4_confirmed,'tn_districts':tn_districts,'tot_districts':tot_districts})
+  key=5
+  for i in range(0,len(statewise)):
+      for j in range(0,len(statewise)-i-1):
+          if int(statewise[j]['confirmed']) < int(statewise[j+1]['confirmed']) :
+              temp=statewise[j]
+              statewise[j]=statewise[j+1]
+              statewise[j+1]=temp
 
+  for i in range(0,len(statewise)):
+      if int(statewise[i]['confirmed']) ==0 :
+          key=i
+          break
+  statewise=statewise[:key]
+
+  return render(request, 'index.html',{'total_india':total_india,'active_india':active_india,'recovered_india':recovered_india,'dead_india':dead_india,'updated_time_india':updated_time_india, 'total_tamilnadu':total_tamilnadu,'active_tamilnadu':active_tamilnadu,'recovered_tamilnadu':recovered_tamilnadu,'dead_tamilnadu':dead_tamilnadu, 'total_world':total_world, 'world_active_cases':world_active_cases, 'total_world_death':total_world_death,'total_world_recovered':total_world_recovered,'today':today,'yesterday':yesterday,'yesterday1':yesterday1,'yesterday2':yesterday2,'yesterday3':yesterday3,'yesterday4':yesterday4,'today_case':today_case,'sterday_confirmed':sterday_confirmed,'sterday1_confirmed':sterday1_confirmed,'sterday2_confirmed':sterday2_confirmed,'sterday3_confirmed':sterday3_confirmed,'sterday4_confirmed':sterday4_confirmed,'tn_districts':tn_districts,
+  'sterday_recovered':sterday_recovered,'sterday1_recovered':sterday1_recovered,'sterday2_recovered':sterday2_recovered,'sterday3_recovered':sterday3_recovered,'sterday4_recovered':sterday4_recovered,'today_recovered':today_recovered,
+  'sterday_death':sterday_death,'sterday1_death':sterday1_death,'sterday2_death':sterday2_death,'sterday3_death':sterday3_death,'sterday4_death':sterday4_death,'today_death':today_death,
+  'y1':y1,'y2':y2,'y3':y3,'y4':y4,'y5':y5,'statewise':statewise})
